003a0f454a79650c1dd193e232ef5cf50343b8cb
Fabio Nelson Usuga Silva <fusuga@integral.com.co>
2023-04-25 15:50:30 -0500

    Implement blobContainerClient.GetBlobsAsync()

--- AzureLabs/BlobApp/Program.cs
+++ AzureLabs/BlobApp/Program.cs
@@ -7,12 +7,29 @@ string blobName = "ScriptLab1.sql";
 string filepath = @"C:\\Users\faneu\\OneDrive\\Escritorio\\ScriptLab1.txt";
 
 
+/// <summary>
+/// Use this for create a container
+/// </summary>
 //BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 //var result = await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
 
-BlobContainerClient blobServiceClient = new BlobContainerClient(connectionString,containerName);
-var blobClient = blobServiceClient.GetBlobClient(blobName);
-var result = await blobClient.UploadAsync(filepath, true);
+
+/// <summary>
+/// Use this to upload file in a container
+/// </summary>
+//BlobContainerClient blobServiceClient = new BlobContainerClient(connectionString,containerName);
+//var blobClient = blobServiceClient.GetBlobClient(blobName);
+//var result = await blobClient.UploadAsync(filepath, true);
 
 
-Console.WriteLine("Uploaded the blob successfully");
+/// <summary>
+/// Use this to upload file in a container
+/// </summary>
+//BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
+//await foreach(BlobItem item in blobContainerClient.GetBlobsAsync())
+//{
+//    Console.WriteLine(item.Name);
+//}
+
+
+//Console.WriteLine("Uploaded the blob successfully");
