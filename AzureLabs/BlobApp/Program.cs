caf30f9c0fbcbcbd64ad14f7b136cb8566a12809
Fabio Nelson Usuga Silva <fusuga@integral.com.co>
2023-04-27 15:27:58 -0500

    example of tableStorage AddEntity, ReadEntity, DeleteEntity,UpdateEntity

--- AzureLabs/BlobApp/Program.cs
+++ AzureLabs/BlobApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using BlobApp;
 
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstoragelab1793;AccountKey=bcpS2tbd1n7jNwCXuwemYVwEgO0OB8WRMQvNd+Webo/IxNRoMmw8pDqsLlMz40Hx2zGjRkvUtdeJ+AStvm+eYw==;EndpointSuffix=core.windows.net";
 string containerName = "script";
@@ -8,22 +9,22 @@ string filepath = @"C:\\Users\faneu\\OneDrive\\Escritorio\\ScriptLab1.txt";
 
 
 /// <summary>
-/// Use this for create a container
+/// 1. Use this for create a container
 /// </summary>
 //BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 //var result = await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.Blob);
 
 
 /// <summary>
-/// Use this to upload file in a container
+/// 2. Use this to upload file in a container
 /// </summary>
-//BlobContainerClient blobServiceClient = new BlobContainerClient(connectionString,containerName);
+//BlobContainerClient blobServiceClient = new BlobContainerClient(connectionString, containerName);
 //var blobClient = blobServiceClient.GetBlobClient(blobName);
 //var result = await blobClient.UploadAsync(filepath, true);
-
+//Console.WriteLine("Uploaded the blob successfully");
 
 /// <summary>
-/// Use this to upload file in a container
+/// 3. Use interate in each file.
 /// </summary>
 //BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
 //await foreach(BlobItem item in blobContainerClient.GetBlobsAsync())
@@ -31,5 +32,54 @@ string filepath = @"C:\\Users\faneu\\OneDrive\\Escritorio\\ScriptLab1.txt";
 //    Console.WriteLine(item.Name);
 //}
 
+/// <summary>
+/// 4. DownLoad file from the storage account.
+/// </summary>
+//BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
+//var resultdownLoad = await blobClient.DownloadToAsync(filepath);
+//Console.WriteLine("The blob was downLoad successfully.");
 
-//Console.WriteLine("Uploaded the blob successfully");
+
+/// <summary>
+/// 5. Set and Get metaData to a Blob.
+/// </summary>
+//await SetMetaData();
+//await GetMetadata();
+
+//async Task SetMetaData()
+//{
+//    string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstoragelab1793;AccountKey=bcpS2tbd1n7jNwCXuwemYVwEgO0OB8WRMQvNd+Webo/IxNRoMmw8pDqsLlMz40Hx2zGjRkvUtdeJ+AStvm+eYw==;EndpointSuffix=core.windows.net";
+//    string containerName = "script";
+//    string blobName = "ScriptLab1.sql";
+//    BlobClient blobClient = new BlobClient(connectionString,containerName,blobName);
+
+//    IDictionary<string, string> metadata = new Dictionary<string, string>();
+//    metadata.Add("Department", "Logistic");
+//    metadata.Add("Application", "App#1");
+//    var result = await blobClient.SetMetadataAsync(metadata);
+
+//    Console.WriteLine($"metaData setting correctly");
+
+//}
+
+//async Task GetMetadata()
+//{
+//    string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstoragelab1793;AccountKey=bcpS2tbd1n7jNwCXuwemYVwEgO0OB8WRMQvNd+Webo/IxNRoMmw8pDqsLlMz40Hx2zGjRkvUtdeJ+AStvm+eYw==;EndpointSuffix=core.windows.net";
+//    string containerName = "script";
+//    string blobName = "ScriptLab1.sql";
+//    BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
+//    BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+
+//    foreach(var metaValue in blobProperties.Metadata)
+//    {
+//        Console.WriteLine($"Key: {metaValue.Key}. Value: {metaValue.Value}");
+//    }
+//} 
+
+
+/// <summary>
+/// 6. Table Storage
+/// </summary>
+/// 
+TableStorageExample tableStorageExample = new TableStorageExample();
+tableStorageExample.TestTableStorage();
---
+++ AzureLabs/BlobApp/TableStorageExample.cs
@@ -0,0 +1,59 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlobApp
+{
+    public class TableStorageExample
+    {
+        string connectionString = "DefaultEndpointsProtocol=https;AccountName=appstoragelab1793;AccountKey=bcpS2tbd1n7jNwCXuwemYVwEgO0OB8WRMQvNd+Webo/IxNRoMmw8pDqsLlMz40Hx2zGjRkvUtdeJ+AStvm+eYw==;EndpointSuffix=core.windows.net";
+        string tableName = "Orders";
+
+        public void TestTableStorage()
+        {
+            //AddEntity("06","Laptop",80);
+            //QueryEntity("Laptop");
+            DeleteEntity("Mobile", "05");
+
+        }
+
+        private void AddEntity(string orderId,string category,int quantity)
+        {
+            TableClient tableClient = new TableClient(connectionString, tableName);
+
+            TableEntity keyValuePairs = new TableEntity(orderId, category)
+            {
+                {"quantity",quantity }
+            };
+
+            tableClient.AddEntity(keyValuePairs);
+            Console.WriteLine($"Added Entity with order ID {orderId}");
+        }
+
+        private void QueryEntity(string category)
+        {
+            TableClient tableClient = new TableClient(connectionString, tableName);
+
+            Pageable<TableEntity> result = tableClient.Query<TableEntity>(e=>e.RowKey==category);
+            foreach(TableEntity tableEntity in result)
+            {
+                Console.WriteLine("Order Id = "+tableEntity.PartitionKey);
+            }
+        }
+
+        private void DeleteEntity(string category,string orderId)
+        {
+            TableClient tableClient = new TableClient(connectionString, tableName);
+            tableClient.DeleteEntity(category, orderId);
+            Console.WriteLine("The Entity is Deleted");
+        }
+
+
+
+    }
+}
