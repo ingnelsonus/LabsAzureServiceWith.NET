0c15ce72c2145c7d99836b3eb54464e73007fc0f
Fabio Nelson Usuga Silva <fusuga@integral.com.co>
2023-05-03 15:32:19 -0500

    Add Items on to a container with cosmosDB

--- AzureLabs/CosmosDB/Program.cs
+++ AzureLabs/CosmosDB/Program.cs
@@ -1,10 +1,33 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using CosmosDB;
+using Microsoft.Azure.Cosmos;
 
 string cosmosEndpointUri = "https://faneusnosqllab01.documents.azure.com:443/";
 string cosmosDBKey = "CZzMa5AiWuhldlCc3gpFhyoc0es4vHSTCqTd5IJPrWnOjMdYmr2zH5G6u8AG4OmQhpj6qioGuJyWACDbPe2ONQ==";
+string databaseName = "appdb";
+string containerName = "Orders";
 
+/// <summary>
+/// Create dataBase on CosmosDB
+/// </summary>
 //await CreateDataBase("appdb");
-await CreateContainer("appdb","Orders","/category");
+
+
+/// <summary>
+/// Create Container on CosmosDB
+/// </summary>
+//await CreateContainer("appdb","Orders","/category");
+
+
+/// <summary>
+/// Add Items in a container
+/// </summary>
+/// 
+
+await AddItem("01","Laptop",100);
+await AddItem("02", "Mobile", 100);
+await AddItem("03", "Desktop", 100);
+await AddItem("04", "Laptop", 100);
+
 
 async Task CreateDataBase(string databaseName)
 {
@@ -21,4 +44,24 @@ async Task CreateContainer(string databaseName, string containerName,string part
     Database database = cosmosClient.GetDatabase(databaseName);
     var result = await database.CreateContainerAsync(containerName, partitionKey);
     Console.WriteLine($"Container {containerName} created on db {databaseName}");
+}
+
+async Task AddItem(string orderId,string category,int quantity)
+{
+    CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+
+    Database database = cosmosClient.GetDatabase(databaseName);
+    Container container = database.GetContainer(containerName);
+
+    Order order = new Order()
+    {
+        id =Guid.NewGuid().ToString(),
+        orderId = orderId,
+        category = category,
+        quantity = quantity
+    };
+
+    var response =await container.CreateItemAsync<Order>(order,new PartitionKey(category));
+    Console.WriteLine("Added item with orderID: " + orderId);
+
 }
